ea56aaa861256684ba71aec9fce93b8ae6a4f08f
EvgenyWas <evgeniywas@gmail.com>
2023-12-24 21:53:38 +0000

    feat: created configs/properties

---
+++ configs/properties.ts
@@ -0,0 +1,13 @@
+export const NAV_PUBLIC_LINKS = [
+  { path: '/about', title: 'About' },
+  { path: '/articles', title: 'Articles' },
+  { path: '/topics', title: 'Topics' },
+];
+export const NAV_USER_LINKS = [
+  { path: '/profile', title: 'Profile', icon: 'fas fa-user' },
+  { path: '/profile/favorites', title: 'Favorites', icon: 'fas fa-star' },
+  { path: '/profile/comments', title: 'Comments', icon: 'fas fa-comments' },
+  { path: '/profile/edit', title: 'Edit', icon: 'fas fa-pen' },
+];
+
+export const MIN_USER_NAME_LENGTH = 3;
