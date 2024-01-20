6ff15a8c3bcaf067ab4457c666e4a86cc544a038
EvgenyWas <evgeniywas@gmail.com>
2024-01-20 19:03:04 +0000

    feat: update route for storage

--- server/routes/storage/[...].ts
+++ server/routes/storage/[...].ts
@@ -1,8 +1,28 @@
+import path from 'node:path';
+
+// eslint-disable-next-line import/named
 import { v2 as cloudinary } from 'cloudinary';
 
 export default defineEventHandler((event) => {
-  const filePath = event.path.replace('/storage', '');
-  const assetUrl = cloudinary.url(filePath);
+  let assetUrl;
+  try {
+    const { req } = event.node;
+    const { pathname, searchParams } = new URL(req.url ?? '', `http://${req.headers.host}`);
+    const pathBase = path.parse(pathname).base;
+    assetUrl = cloudinary.url(pathBase, {
+      quality: 'auto',
+      fetch_format: 'auto',
+      width: searchParams.get('width'),
+      height: searchParams.get('height'),
+      gravity: searchParams.get('gravity'),
+      crop: searchParams.get('crop'),
+    });
+  } catch (error) {
+    return sendError(
+      event,
+      createError({ statusCode: 500, statusMessage: 'Internal Server Error', fatal: false, unhandled: false }),
+    );
+  }
 
   return sendProxy(event, assetUrl);
 });
