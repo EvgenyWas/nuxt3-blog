98ef6a15df6adfac7ce95ec295e1d68c34616ed7
EvgenyWas <evgeniywas@gmail.com>
2024-03-03 19:31:50 +0000

    refactor: updated api client composable

--- composables/useAPIClient.ts
+++ composables/useAPIClient.ts
@@ -1,14 +1,17 @@
+import type { AuthState } from '~/types/states';
+
 export default function useAPIClient<T>(
-  ...[request, opts]: Parameters<typeof useFetch<T>>
+  ...[request, opts = {}]: Parameters<typeof useFetch<T>>
 ): ReturnType<typeof useFetch<T>> {
+  const auth = useAuth();
+  const userAgent = process.server ? useRequestHeaders()['user-agent'] : navigator.userAgent;
+
   return useFetch<T>(request, {
     ...opts,
     retry: 2,
     retryStatusCodes: [401, 408, 409, 425, 429, 500, 502, 503, 504],
-    ignoreResponseError: false,
     onRequest({ options }) {
-      console.log('request', options);
-      const token = useAuth().value.token;
+      const token = auth.value.token;
       options.headers = new Headers(options.headers || {});
       if (token) {
         options.headers.append('Authorization', `Bearer ${token}`);
@@ -18,10 +21,9 @@ export default function useAPIClient<T>(
     },
     async onResponseError({ response }) {
       if (response.status === 401) {
-        const auth = useAuth();
-        const userAgent = process.server ? useRequestHeaders()['User-Agent'] ?? '' : navigator.userAgent;
         try {
-          const { token } = await $fetch<{ token: string }>('/api/auth/token/refresh', {
+          const { token } = await $fetch<Pick<AuthState, 'token'>>('/api/auth/token/refresh', {
+            method: 'POST',
             headers: { 'User-Agent': userAgent },
           });
           auth.value = { token, authorized: true };
