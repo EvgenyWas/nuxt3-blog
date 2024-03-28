4113c8292ff8946e9ff4c86083e38e7db3ca9d8b
EvgenyWas <evgeniywas@gmail.com>
2024-03-28 21:02:50 +0000

    feat: added github auth link

---
+++ components/AuthProviders.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="d-flex ga-3 justify-center">
+    <NuxtLink
+      :to="githubOAuthURL"
+      aria-label="log in with GitHub"
+      external
+    >
+      <VIcon
+        icon="fab fa-github-square"
+        size="50"
+      />
+    </NuxtLink>
+    <NuxtLink
+      :to="googleOAuthURL"
+      aria-label="log in with Google"
+      external
+    >
+      <VIcon
+        icon="fab fa-google-plus-square"
+        size="50"
+      />
+    </NuxtLink>
+  </div>
+</template>
+
+<script setup lang="ts">
+const runtimeConfig = useRuntimeConfig();
+
+const githubOAuthURL = `https://github.com/login/oauth/authorize?scope=read:user&client_id=${runtimeConfig.public.githubClientId}`;
+const googleOAuthURL = '';
+</script>
+
+<style scoped></style>
