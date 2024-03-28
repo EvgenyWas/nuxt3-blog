8c9c7ef29ce3571cc263e84953bff3850c6d83ec
EvgenyWas <evgeniywas@gmail.com>
2024-03-28 21:03:12 +0000

    feat: created server/env.d.ts

---
+++ server/env.d.ts
@@ -0,0 +1,15 @@
+declare global {
+  namespace NodeJS {
+    interface ProcessEnv {
+      NODE_ENV: 'development' | 'production' | 'test';
+      PORT?: string;
+      PWD: string;
+      GITHUB_CLIENT_SECRET: string;
+      NUXT_PUBLIC_GITHUB_CLIENT_ID: string;
+      JWT_ACCESS_SECRET: string;
+      JWT_REFRESH_SECRET: string;
+    }
+  }
+}
+
+export {};
