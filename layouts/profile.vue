27c0445d358067d1b9d5e4abb6f7ee579ead4869
EvgenyWas <evgeniywas@gmail.com>
2024-04-03 19:59:22 +0100

    feat: added profile layout skeleton

---
+++ layouts/profile.vue
@@ -0,0 +1,20 @@
+<template>
+  <VLayout class="rounded rounded-md">
+    <VNavigationDrawer>
+      <VList>
+        <VListItem title="Navigation drawer" />
+      </VList>
+    </VNavigationDrawer>
+
+    <VAppBar title="Application bar"></VAppBar>
+
+    <VMain
+      class="d-flex align-center justify-center"
+      style="min-height: 300px"
+    >
+      <slot />
+    </VMain>
+  </VLayout>
+</template>
+
+<script setup lang="ts"></script>
