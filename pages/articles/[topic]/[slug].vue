d02c6c38f87ba153a3468afb7d616783b7f96080
EvgenyWas <evgeniywas@gmail.com>
2024-05-20 22:14:47 +0100

    feat: create article page

---
+++ pages/articles/[topic]/[slug].vue
@@ -0,0 +1,194 @@
+<template>
+  <div class="container">
+    <article class="article">
+      <div class="d-flex flex-column justify-center align-center mb-8 mb-md-12">
+        <h1 class="mb-1 text-h5 text-md-h4 text-center">{{ article?.title }}</h1>
+        <p
+          v-if="article?.author"
+          class="text-subtitle-1"
+        >
+          {{ article.author }}
+        </p>
+        <NuxtLink
+          v-if="article?.src"
+          :to="article.src"
+          class="text-caption text-decoration-underline"
+          target="_blank"
+          external
+        >
+          Source article
+        </NuxtLink>
+        <img
+          v-if="article?.image"
+          :src="article.image"
+          :alt="article?.title"
+          class="w-75"
+        />
+      </div>
+
+      <ContentRenderer :value="article" />
+    </article>
+
+    <VContainer
+      class="px-0"
+      tag="section"
+    >
+      <VRow>
+        <VCol cols="6">
+          <VBtn
+            v-if="prevSibling"
+            variant="plain"
+            prepend-icon="fas fa-chevron-left"
+            rounded="md"
+            class="text-body-1"
+            @click="onPrevClick"
+          >
+            {{ prevSibling.title }}
+          </VBtn>
+        </VCol>
+
+        <VCol
+          cols="6"
+          class="d-flex justify-end"
+        >
+          <VBtn
+            v-if="nextSibling"
+            variant="plain"
+            append-icon="fas fa-chevron-right"
+            rounded="md"
+            class="text-body-1"
+            @click="onNextClick"
+          >
+            {{ nextSibling.title }}
+          </VBtn>
+        </VCol>
+      </VRow>
+    </VContainer>
+  </div>
+</template>
+
+<script setup lang="ts">
+import type { ArticleContent } from '~/types/responses';
+
+type ArticlePageTransition = 'slide-left' | 'slide-right' | 'fade';
+
+definePageMeta({
+  pageTransition: {
+    name: 'fade',
+    mode: 'out-in',
+  },
+  middleware(to) {
+    const pageTransition = useState<ArticlePageTransition>('article-page-transition');
+    if (to.meta.pageTransition && typeof to.meta.pageTransition !== 'boolean' && pageTransition.value) {
+      to.meta.pageTransition.name = pageTransition.value;
+    }
+  },
+});
+
+const pageTransition = useState<ArticlePageTransition>('article-page-transition');
+const route = useRoute();
+
+const { data: article, error } = await useAsyncData(route.path, () =>
+  queryContent<ArticleContent>(route.path).sort({ id: 1, $numeric: true }).findOne(),
+);
+if (!article.value || error.value) {
+  throw createError({ statusCode: 404, data: { to: '/articles' }, fatal: true });
+}
+
+useSeoMeta({ title: article.value?.title, description: article.value?.description });
+
+const { data: siblings } = await useLazyAsyncData(
+  () =>
+    queryContent<ArticleContent>()
+      .only(['_path', 'title'])
+      .sort({ id: 1, $numeric: true })
+      .where({ _path: { $contains: `/articles/${route.params.topic}` } })
+      .findSurround(route.path),
+  { deep: false },
+);
+
+const prevSibling = computed(() => siblings.value?.[0] as ArticleContent | undefined);
+
+const nextSibling = computed(() => siblings.value?.[1] as ArticleContent | undefined);
+
+const onPrevClick = () => {
+  pageTransition.value = 'slide-right';
+  navigateTo(prevSibling.value?._path);
+};
+
+const onNextClick = () => {
+  pageTransition.value = 'slide-left';
+  navigateTo(nextSibling.value?._path);
+};
+
+onBeforeRouteLeave((_, from) => {
+  pageTransition.value = 'fade';
+  if (from.meta.pageTransition && typeof from.meta.pageTransition !== 'boolean') {
+    from.meta.pageTransition.name = 'fade';
+  }
+});
+</script>
+
+<style lang="scss">
+.container {
+  max-width: 100%;
+}
+
+.v-application.v-theme--light {
+  .article {
+    pre.shiki {
+      background-color: rgb(var(--v-theme-on-surface));
+    }
+  }
+}
+
+.v-application.v-theme--dark {
+  .article {
+    pre.shiki {
+      background-color: rgb(var(--v-theme-surface));
+    }
+  }
+}
+
+.article {
+  & * {
+    margin-bottom: 8px;
+  }
+
+  pre.shiki {
+    max-width: 100%;
+    margin: 12px 0 20px;
+    padding: 16px;
+    border-radius: 8px;
+    overflow-x: auto;
+  }
+
+  img {
+    max-width: 100%;
+    height: auto;
+    max-height: 500px;
+    object-fit: contain;
+  }
+
+  code {
+    background-color: rgba(var(--v-theme-secondary), 0.3);
+  }
+
+  p {
+    @extend .text-h1 !optional;
+  }
+
+  ol {
+    list-style-type: decimal;
+  }
+
+  ul {
+    list-style-type: disc;
+  }
+
+  ol,
+  ul {
+    padding-left: 30px;
+  }
+}
+</style>
--- types/responses.ts
+++ types/responses.ts
@@ -1,5 +1,7 @@
+import type { MarkdownParsedContent } from '@nuxt/content/types';
 import type { AuthState } from './states';
 import type { Profile } from './user';
+import type { ARTICLE_KEYWORDS } from '~/configs/properties';
 
 export interface SignupResponse {
   token: AuthState['token'];
@@ -14,3 +16,12 @@ export interface TokenRefreshResponse {
 }
 
 export type WhoamiResponse = Profile;
+
+export interface ArticleContent extends MarkdownParsedContent {
+  title: string;
+  description: string;
+  image?: string;
+  src?: string;
+  author?: string;
+  keywords: Array<(typeof ARTICLE_KEYWORDS)[number]>;
+}
