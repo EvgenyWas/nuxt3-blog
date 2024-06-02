53e1e5a8ee82019579eea333be5a8f9387e4e6ba
EvgenyWas <evgeniywas@gmail.com>
2024-06-02 15:41:12 +0100

    feat: add more queries to storage server route

--- server/routes/storage/[...].ts
+++ server/routes/storage/[...].ts
@@ -9,11 +9,12 @@ export default defineEventHandler((event) => {
     const path = pathname.replace('/storage/', '');
     assetUrl = cloudinary.url(path, {
       quality: 'auto',
-      fetch_format: 'auto',
+      fetch_format: 'webp',
       width: searchParams.get('width'),
       height: searchParams.get('height'),
       gravity: searchParams.get('gravity'),
       crop: searchParams.get('crop'),
+      aspect_ratio: searchParams.get('aspect_ratio'),
     });
   } catch (error) {
     return sendError(event, createError({ statusCode: 500, statusMessage: 'Internal Server Error', unhandled: false }));
