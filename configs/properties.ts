04027bc5ff7f4435ea84f9d2c08475afe722eb1d
EvgenyWas <evgeniywas@gmail.com>
2024-06-10 18:21:30 +0100

    feat: update properties

--- configs/properties.ts
+++ configs/properties.ts
@@ -50,3 +50,4 @@ export const ARTICLE_TOPICS = [
 ];
 export const ARTICLE_KEYWORDS = ['api', 'architecture', 'design', 'fundamentals', 'perfomance', 'refactoring'] as const;
 export const MAX_ARTICLE_RATE = 5;
+export const ARTICLE_RATE_MAX_AGE = 30 * 24 * 60 * 60;
