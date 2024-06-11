500bb0f6570abfbd66c856655a1c1d7c45d7b138
EvgenyWas <evgeniywas@gmail.com>
2024-06-11 06:57:23 +0100

    feat: connect article stats endpoints to article page

--- pages/articles/[topic]/[slug].vue
+++ pages/articles/[topic]/[slug].vue
@@ -58,16 +58,29 @@
     >
       <VRow v-if="stats || !statsPending">
         <VCol class="d-flex flex-column align-center justify-center">
-          <div class="text-h5 mb-2">Rating overview</div>
-          <div class="text-h4">
-            {{ stats?.rate }}
+          <h4 class="text-h5 mb-2">⭐ Rating overview ⭐</h4>
+          <p
+            v-if="stats?.rate"
+            class="text-h4"
+          >
+            {{ stats.rate }}
             <span class="text-h6 ml-n3">&nbsp;/ {{ MAX_ARTICLE_RATE }}</span>
-          </div>
+          </p>
+          <p v-else>Be the first to rate this article 😉</p>
           <VRating
-            :model-value="3.5"
+            v-if="!rateCookie"
+            v-model="rateModelValue"
+            item-aria-label="rate {0} of {1}"
             color="yellow-darken-3"
+            hover
+            @update:model-value="rateArticle"
           />
-          <div class="px-3">{{ stats?.ratings }} ratings</div>
+          <p
+            v-if="stats?.ratings"
+            class="px-3"
+          >
+            {{ stats.ratings }} ratings ✨
+          </p>
         </VCol>
       </VRow>
 
@@ -110,12 +123,15 @@
         </VCol>
       </VRow>
     </VContainer>
+
+    <div id="views-observer" />
   </div>
 </template>
 
 <script setup lang="ts">
-import { MAX_ARTICLE_RATE } from '~/configs/properties';
-import type { ArticleContent } from '~/types/responses';
+import { debounce } from 'lodash-es';
+import { ARTICLE_RATE_MAX_AGE, MAX_ARTICLE_RATE } from '~/configs/properties';
+import type { ArticleContent, ArticlesStats } from '~/types/responses';
 
 type ArticlePageTransition = 'slide-left' | 'slide-right' | 'fade';
 
@@ -134,6 +150,14 @@ definePageMeta({
 
 const pageTransition = useState<ArticlePageTransition>('article-page-transition');
 const route = useRoute();
+const { openSuccessfulSnackbar, openErrorSnackbar } = useSnackbar();
+
+const topic = route.params.topic;
+const title = route.params.slug;
+
+const rateCookie = useCookie(`article-rate-${topic}-${title}`, { maxAge: ARTICLE_RATE_MAX_AGE });
+
+const rateModelValue = ref<number>(5);
 
 const { data: article, error } = await useAsyncData(route.path, () =>
   queryContent<ArticleContent>(route.path).sort({ id: 1, $numeric: true }).findOne(),
@@ -144,11 +168,8 @@ if (!article.value || error.value) {
 
 useSeoMeta({ title: article.value?.title, description: article.value?.description });
 
-// TODO: add stats endpoint
-const { data: stats, pending: statsPending } = await useLazyAsyncData<{ views: number; rate: number; ratings: number }>(
-  () => {
-    return new Promise((resolve) => setTimeout(() => resolve({ views: 200, rate: 3.3, ratings: 3360 }), 2000));
-  },
+const { data: stats, pending: statsPending } = await useLazyAsyncData(
+  () => $fetch<ArticlesStats>('/api/public/article/stats', { params: { topic, title } }),
   { deep: false },
 );
 
@@ -157,7 +178,7 @@ const { data: siblings } = await useLazyAsyncData(
     queryContent<ArticleContent>()
       .only(['_path', 'title'])
       .sort({ id: 1, $numeric: true })
-      .where({ _path: { $contains: `/articles/${route.params.topic}` } })
+      .where({ _path: { $contains: `/articles/${topic}` } })
       .findSurround(route.path),
   { deep: false },
 );
@@ -165,6 +186,16 @@ const { data: siblings } = await useLazyAsyncData(
 const prevSibling = computed(() => siblings.value?.[0] as ArticleContent | undefined);
 
 const nextSibling = computed(() => siblings.value?.[1] as ArticleContent | undefined);
+
+const rateArticle = debounce(async (rate: number | string) => {
+  try {
+    await $fetch('/api/public/article/stats/rate', { method: 'PUT', params: { topic, title }, body: { rate } });
+    rateCookie.value = 'true';
+    openSuccessfulSnackbar('Thank you! Your rating has been recorded.');
+  } catch (error) {
+    openErrorSnackbar(error);
+  }
+}, 1000);
 
 const onPrevClick = () => {
   pageTransition.value = 'slide-right';
@@ -180,6 +211,24 @@ onBeforeRouteLeave((_, from) => {
   pageTransition.value = 'fade';
   if (from.meta.pageTransition && typeof from.meta.pageTransition !== 'boolean') {
     from.meta.pageTransition.name = 'fade';
+  }
+});
+
+onMounted(() => {
+  const viewsObserverTarget = document.querySelector('#views-observer');
+  if (viewsObserverTarget) {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            observer.unobserve(viewsObserverTarget);
+            $fetch('/api/public/article/stats/views', { method: 'PUT', params: { topic, title } });
+          }
+        });
+      },
+      { root: null, threshold: 1 },
+    );
+    observer.observe(viewsObserverTarget);
   }
 });
 </script>
@@ -227,5 +276,13 @@ onBeforeRouteLeave((_, from) => {
   h6 {
     margin-bottom: 12px;
   }
+}
+
+#views-observer {
+  position: absolute;
+  top: 60%;
+  left: 0;
+  width: 100%;
+  height: 1px;
 }
 </style>
