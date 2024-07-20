efd0f6c933ddb44b33075166f294e2cfa01b0b64
EvgenyWas <evgeniywas@gmail.com>
2024-07-20 18:27:32 +0100

    feat: create user profile put endpoint

---
+++ server/api/user/[id]/profile/index.put.ts
@@ -0,0 +1,33 @@
+import Profile from '~/server/models/user/profile.model';
+import { userProfileSchema } from '~/server/schemas';
+import { isMongooseError } from '~/server/utils';
+import { Profile as ProfileType } from '~/types/user';
+import { isZodError } from '~/utils/helpers';
+
+export default defineEventHandler(async (event) => {
+  const id = getRouterParam(event, 'id');
+  if (!id) {
+    return sendError(event, createError({ statusCode: 400, statusMessage: 'User ID is not provided' }));
+  }
+
+  let payload: ProfileType;
+  try {
+    payload = await readValidatedBody(event, (body) => userProfileSchema.parse(body));
+  } catch (error) {
+    return sendError(
+      event,
+      createError({ statusCode: 400, statusMessage: isZodError(error) ? error.message : 'Payload is invalid' }),
+    );
+  }
+
+  try {
+    const profile = await Profile.updateOne(payload);
+
+    return profile;
+  } catch (error) {
+    return sendError(
+      event,
+      createError({ statusCode: 400, statusMessage: isMongooseError(error) ? error.message : 'Payload is invalid' }),
+    );
+  }
+});
--- server/schemas.ts
+++ server/schemas.ts
@@ -1,6 +1,7 @@
 import { z } from 'zod';
 
-import { AUTH_PROVIDERS } from '~/configs/properties';
+import { AUTH_PROVIDERS, MAX_USER_SOCIALS, MIN_USER_NAME_LENGTH } from '~/configs/properties';
+import { base64Validator, emailValidator, phoneValidator, urlValidator } from '~/utils/validators';
 
 export const userIdentitySchema = z
   .object({
@@ -9,3 +10,19 @@ export const userIdentitySchema = z
   })
   .required()
   .strict();
+
+export const userProfileSchema = z
+  .object({
+    id: z.string().or(z.null()),
+    name: z.string().min(MIN_USER_NAME_LENGTH),
+    email: emailValidator,
+    avatar: z.string().refine((value) => !base64Validator.safeParse(value).success),
+    description: z.string().optional(),
+    address: z.string().optional(),
+    phone: phoneValidator.optional(),
+    socials: z.array(urlValidator.or(z.literal(''))).max(MAX_USER_SOCIALS),
+    createdAt: z.string().optional(),
+    updatedAt: z.string().optional(),
+  })
+  .required()
+  .strict({ message: 'Unknown keys are presented in the payload' });
