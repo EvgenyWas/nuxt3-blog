8b452510f8094085a771ba856307020c4f3c9a5b
EvgenyWas <evgeniywas@gmail.com>
2024-07-27 18:16:47 +0100

    refactor: update user profile avatar put endpoint

--- server/api/user/[id]/profile/avatar/index.put.ts
+++ server/api/user/[id]/profile/avatar/index.put.ts
@@ -1,8 +1,9 @@
 import path from 'node:path';
-
-// eslint-disable-next-line import/named
-import { type UploadApiErrorResponse, v2 as cloudinary } from 'cloudinary';
-import { fileToDataURI } from '~/server/utils';
+import { v2 as cloudinary } from 'cloudinary';
+import formidable from 'formidable';
+import { get, isEmpty } from 'lodash-es';
+import { isCloudinarUploadApiError } from '~/server/utils';
+import { MAX_USER_AVATAR_SIZE } from '~/configs/properties';
 
 export default defineEventHandler(async (event) => {
   const id = getRouterParam(event, 'id');
@@ -10,33 +11,61 @@ export default defineEventHandler(async (event) => {
     return sendError(event, createError({ statusCode: 400, statusMessage: 'User ID is not provided' }));
   }
 
-  let dataURI;
-  let fileName;
+  let filepath;
+  let filename;
   try {
-    const formData = await readFormData(event);
-    const file = formData.get('avatar') as File;
-    dataURI = await fileToDataURI(file);
-    fileName = path.parse(file.name).name;
+    const form = formidable({
+      allowEmptyFiles: false,
+      maxFiles: 1,
+      maxFileSize: MAX_USER_AVATAR_SIZE,
+    });
+    const [, files] = await form.parse(event.node.req);
+    if (isEmpty(files) || !files.avatar) {
+      return sendError(event, createError({ statusCode: 400, statusMessage: 'File is required' }));
+    }
+
+    const file = files.avatar[0];
+    if (!file.mimetype?.startsWith('image/')) {
+      return sendError(event, createError({ statusCode: 400, statusMessage: 'File is not a valid image' }));
+    }
+
+    filepath = file.filepath;
+    filename = path.parse(file.originalFilename ?? file.newFilename).name;
   } catch (error) {
-    return sendError(
-      event,
-      createError({ statusCode: 400, statusMessage: 'Attached file is missed or it is not valid.' }),
-    );
+    if (get(error, 'code') === formidable.errors.biggerThanMaxFileSize) {
+      return sendError(
+        event,
+        createError({
+          statusCode: 400,
+          statusMessage: `Attached file is bigger than ${MAX_USER_AVATAR_SIZE / 1024 / 1024} Mb`,
+        }),
+      );
+    }
+
+    if (get(error, 'code') === formidable.errors.missingContentType) {
+      return sendError(event, createError({ statusCode: 400, statusMessage: 'Attached file is missing type' }));
+    }
+
+    if (get(error, 'code') === formidable.errors.noEmptyFiles) {
+      return sendError(event, createError({ statusCode: 400, statusMessage: 'Attached file is empty' }));
+    }
+
+    return sendError(event, createError({ statusCode: 400, statusMessage: 'Attached file is invalid' }));
   }
 
   try {
-    const img = await cloudinary.uploader.upload(dataURI, {
+    const img = await cloudinary.uploader.upload(filepath, {
       resource_type: 'image',
-      public_id: fileName,
+      public_id: filename,
       folder: `users/${id}`,
+      transformation: { format: 'webp' },
     });
 
-    return `/storage/users/${id}/${img.public_id}`;
+    return `/storage/${img.public_id}`;
   } catch (error) {
-    const cloudinaryError = error as UploadApiErrorResponse;
-    return sendError(
-      event,
-      createError({ statusCode: cloudinaryError.http_code, statusMessage: cloudinaryError.message }),
-    );
+    const errorInput = isCloudinarUploadApiError(error)
+      ? { statusCode: error.http_code, statusMessage: error.message }
+      : { statusCode: 500, statusMessage: 'Internal Server Error' };
+    return sendError(event, createError(errorInput));
   }
 });
