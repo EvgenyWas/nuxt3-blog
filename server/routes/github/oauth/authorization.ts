1191a4756d1de906393705f67a7a3c0b61c81ed4
EvgenyWas <evgeniywas@gmail.com>
2024-08-22 08:51:51 +0100

    refactor: add redirect url to octokit service

--- server/routes/github/oauth/authorization.ts
+++ server/routes/github/oauth/authorization.ts
@@ -1,5 +1,12 @@
 import { octokitOAuthApp } from '~/server/services';
 
 export default defineEventHandler((event) => {
-  return sendRedirect(event, octokitOAuthApp.getWebFlowAuthorizationUrl({ allowSignup: true }).url, 301);
+  return sendRedirect(
+    event,
+    octokitOAuthApp.getWebFlowAuthorizationUrl({
+      allowSignup: true,
+      redirectUrl: `${process.env.NUXT_PUBLIC_APP_URL}/github/oauth/callback`,
+    }).url,
+    301,
+  );
 });
--- server/services/octokitOAuthApp.ts
+++ server/services/octokitOAuthApp.ts
@@ -4,6 +4,7 @@ const octokitOAuthApp = new OAuthApp({
   clientType: 'github-app',
   clientId: process.env.NUXT_GITHUB_CLIENT_ID,
   clientSecret: process.env.NUXT_GITHUB_CLIENT_SECRET,
+  redirectUrl: `${process.env.NUXT_PUBLIC_APP_URL}/github/oauth/callback`,
 });
 
 export default octokitOAuthApp;
